34632161b3103b8968d13bbf0a92524d27a51576
Ieuan <193008456+Ieuan-P@users.noreply.github.com>
2026-09-11 14:36:42 +0000

    Did some stuff

--- Python Stuff/School Stuff/L6 Theory/11.9.2026.py
+++ Python Stuff/School Stuff/L6 Theory/11.9.2026.py
@@ -1,7 +1,27 @@
 def Q1():
     x = int(input("X: "))
     y = int(input("Y: "))
-    if (x or y) == 10 or (x + y) == 10:
+    if x == 10 or y == 10 or (x + y) == 10:
         print("Yes")
 
-Q1()
+def Q2():
+    n = int(input("Enter a number: "))
+    if n in range(90,111):
+        print("True")
+    else:
+        print("False")
+
+def Q3():
+    n = int(input("Enter a number: "))
+    if n % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+def Q4():
+    UserName = input("Enter your name: ")
+    Date_Of_Birth = input("Enter your date of birth (DD-MM-YYYY): ")
+    Year_Of_Birth = int(Date_Of_Birth.split("-")[2])
+    Month_Of_Birth = int(Date_Of_Birth.split("-")[1])
+    Day_Of_Birth = int(Date_Of_Birth.split("-")[0])
+Q4()
