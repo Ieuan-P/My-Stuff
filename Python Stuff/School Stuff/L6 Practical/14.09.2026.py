a504fe77bbd93f03982229c0ea7353b0c6ee1c3f
Ieuan <193008456+Ieuan-P@users.noreply.github.com>
2026-09-15 11:21:41 +0000

    Updated file 14.09.2026.py again

--- Python Stuff/School Stuff/L6 Practical/14.09.2026.py
+++ Python Stuff/School Stuff/L6 Practical/14.09.2026.py
@@ -16,7 +16,21 @@ Task1()
 def Task2():
     value = random.randint(1, 4000)
     print(f"Value: {value}")
-    roman_numerals = {1000: 'M',900: 'CM',500: 'D',400: 'CD',100: 'C',90: 'XC',50: 'L',40: 'XL',10: 'X',9: 'IX',5: 'V',4: 'IV',1: 'I'}
+    roman_numerals = {
+        1000: 'M',
+        900: 'CM',
+        500: 'D',
+        400: 'CD',
+        100: 'C',
+        90: 'XC',
+        50: 'L',
+        40: 'XL',
+        10: 'X',
+        9: 'IX',
+        5: 'V',
+        4: 'IV',
+        1: 'I'
+        }
     result = ""
     for num in sorted(roman_numerals.keys(), reverse=True):
         while value >= num:
