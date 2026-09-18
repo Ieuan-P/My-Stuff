475b41e20d6aec0cb52ce10fafe5f2bc86412362
Ieuan <193008456+Ieuan-P@users.noreply.github.com>
2026-09-18 15:01:44 +0000

    updated file 18.09.2026.py again

--- Python Stuff/School Stuff/L6 Practical/18.09.2026.py
+++ Python Stuff/School Stuff/L6 Practical/18.09.2026.py
@@ -11,10 +11,29 @@ def Luhn(number):
         checksum += sum(numbers(d * 2))
     return checksum % 10 == 0
 
-CardNumber = input("Enter a credit card number: ")
-CardNumber = CardNumber.replace(" ", "")
+#CardNumber = input("Enter a credit card number: ")
+#CardNumber = CardNumber.replace(" ", "")
 
-if Luhn(CardNumber):
+#if Luhn(CardNumber):
     print("The credit card number is valid.")
-else:
+#else:
     print("The credit card number is invalid.")
+
+def Q14():
+     int1 = int(input("Enter a number: "))
+     int2 = int(input("Enter a number: "))
+     final = 0
+     val1 = int1
+     for i in range(int2):
+          final += val1
+          val1 = val1 * 10 + int1
+     print(f"The final value is: {final}")
+
+#Q14()
+
+def Q15():
+     sentence = str(input("Enter a sentence: "))
+     words = sentence.split()
+     print(f"The longest word in the sentence is: {max(words, key=len)}")
+
+Q15()
