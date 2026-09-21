08863d518b80a9e5680a7f8c0b2f0fca7dcf4407
Ieuan <193008456+Ieuan-P@users.noreply.github.com>
2026-09-21 09:27:11 +0000

    created file 21.09.2026.py

---
+++ Python Stuff/School Stuff/L6 Practical/21.09.2026.py
@@ -0,0 +1,60 @@
+def Q3():
+    n = int(input("Enter a number: "))
+    listn = []
+    for i in range(1,51):
+        if i % n == 0:
+            listn.append(i)
+    print(listn)
+
+#Q3()
+
+def Q4():
+    sentence = input("Enter a sentence: ")
+    sentence_without_spaces = []
+    for word in sentence.split():
+        sentence_without_spaces.append(word)
+    print("".join(sentence_without_spaces))
+
+#Q4()
+
+def Q4Compact():
+    print("".join(input("Enter a Sentence: ").split()))
+
+
+#Q4Compact()
+
+def Q12():
+    Letters_and_Digits = input("Enter a string: ")
+    Letters_in_string = 0
+    Digits_in_string = 0
+    for i in Letters_and_Digits:
+        if i.isalpha():
+            Letters_in_string += 1
+        elif i.isdigit():
+            Digits_in_string += 1
+    print("Letters: "+str(Letters_in_string))
+    print("Digits: "+str(Digits_in_string))
+
+#Q12()
+
+def Q12Compact():
+    Sen = input("Enter a string: ") ; print("Letters: "+str(sum(i.isalpha() for i in Sen)), "Digits: "+str(sum(i.isdigit() for i in Sen)))
+
+#Q12Compact()
+
+def Q13():
+    Username = input("Enter a username: ")
+    if Username == "admin":
+        while True:
+            Password = input("Enter your password: ")
+            if Password == "123":
+                print("Success")
+                break
+            else:
+                print("Invalid password")
+                return Q13()
+    else:
+        print("Invalid username")
+        return Q13()
+
+#Q13()
