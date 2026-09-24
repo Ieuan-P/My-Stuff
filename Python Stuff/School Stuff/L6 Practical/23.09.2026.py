674a55b6c3536ba664b7859262e6ec37de80344c
Ieuan <193008456+Ieuan-P@users.noreply.github.com>
2026-09-24 11:31:32 +0000

    created file

--- Python Stuff/School Stuff/L6 Practical/23.09.2026.py
+++ Python Stuff/School Stuff/L6 Practical/23.09.2026.py
@@ -32,7 +32,92 @@ def P3():
         print(f"You must be 18 years old to vote. Your are {Age} years old. You cannot vote!")
 
 def P4():
+    while True:
+        try:
+            UserName = str(input("Enter Username: "))
+            break
+        except:
+            print("Something went wrong. Try again...")
+            P4()
+    Valid1 = True
+    for i in UserName:
+        if i.isalpha():
+            pass
+        else:
+            Valid = False
+    Valid2 = True
+    if len(UserName) in range (5,16):
+        pass
+    else:
+        Valid2 = False
+    if Valid1 and Valid2:
+        print("Username is valid")
+    else:
+        print("Username is invalid")
 
+def P5():
+    while True:
+        try:
+            Password = str(input("Enter Password: "))
+            break
+        except:
+            print("Something went wrong. Try again...")
+            P5()
+    Contains_Upper = False
+    Contains_Lower = False
+    Contains_Digit = False
+    Contains_Special = False
+    for i in Password:
+        if i.isupper():
+            Contains_Upper = True
+        if i.islower():
+            Contains_Lower = True
+        if i.isdigit():
+            Contains_Digit = True
+        if i.isalnum():
+            Contains_Special = True
+    if Contains_Upper and Contains_Lower and Contains_Digit and Contains_Special:
+        print("Password is valid")
+    else:
+        print("Password is invalid")
+
+def P6():
+    while True:
+        try:
+            Phone = int(input("Enter phone number: "))
+            break
+        except ValueError:
+            print("Only enter numbers...")
+    if len(str(Phone)) == 10:
+        print("Phone number is valid")
+    else:
+        print("Phone number is invalid")
+
+def P7():
+    while True:
+        try:
+            Email = str(input("Enter email: "))
+            break
+        except:
+            print("Something went wrong. Try again...")
+    AtSign = 0
+    for i in len(Email):
+        if Email[i] == "@":
+            AtSign = i
+    if AtSign == 0:
+        print("Email is invalid")
+    Before_AtSign = []
+    for i in range(AtSign - 1):
+        Before_AtSign.append(1,Email[i])
+    After_AtSign = []
+    for i in range(AtSign + 1, len(Email)):
+        After_AtSign.append(Email[i])
+    if Before_AtSign == [] and After_AtSign == []:
+        print("Email is invalid")
+    else:
+        print("Email is valid")
+
+P7()
 
 def MainMenu():
     while True:
@@ -43,4 +128,4 @@ def MainMenu():
             print("Int / String Error")
             
 
-MainMenu()
+#MainMenu()
